ca287a6a811f1eaef4ea7a4578b1f1f5dcf5acb0
TrystanHumann <humanntrystan@hotmail.com>
2018-02-19 23:00:01 -0500

    fix merge conflicts

--- Database/functions/insert_match_sp.sql
+++ Database/functions/insert_match_sp.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE OR REPLACE FUNCTION public.insert_match_sp
 (ev integer, ga integer, sch timestamp without time zone DEFAULT NULL::timestamp without time zone, win integer DEFAULT NULL::integer)
  RETURNS TABLE
@@ -27,34 +26,3 @@ begin
 --RAISE INFO 'Out variable: %', outUserID;
 end
 $function$
-=======
--- drop function public.insert_match_sp(int, int, timestamp, /*timestamp,*/ int)
-CREATE OR REPLACE FUNCTION public.insert_match_sp(ev int, ga int, sch timestamp = null, /*comp timestamp = null,*/ win int = null) returns table(returnid int)
- LANGUAGE plpgsql
-AS $$
-declare fetchID int;
-begin
-	-- Assuming they don't update competitors
-	fetchID := (select m.id from public.matches m where m.event = ev and m.game = ga);
-	if fetchID is null then
-	-- insert
-		insert into public.matches(event, game, scheduled, winner)
-		values (ev, ga, sch, /*comp,*/ win);
-	else 
-		-- update 
-		update public.matches
-		set
-			event = ev,
-			game = ga, 
-			scheduled = sch,
-			--completed = comp, 
-			winner = win  
-		where m.event = event and m.game = game;	
-	end if;
-	return query
-	select m.id as returnid from public.matches m where m.event = ev and m.game = ga;	
-	--RAISE INFO 'Out variable: %', outUserID;
-end $$;
-
-
->>>>>>> d305009f3ebb93b3f7e7c23c87f02ba6be5d6fd7
