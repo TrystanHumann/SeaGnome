0198a1e257e284e525dd194b7a8f1ca0a5398d7c
Tyler Huskins <tylerhuskins@gmail.com>
2018-02-22 18:51:13 -0500

    Made requested changes
    Changed error handling to return Http.error instead of logging to
    console.  Also added comments to make things more apparent.

--- Backend/handlers/background.go
+++ Backend/handlers/background.go
@@ -1,27 +1,31 @@
 package handlers
 
 import (
-	"fmt"
 	"image/png"
 	"net/http"
 	"os"
 )
 
-//stuff for the background
+//Used to change the background of the webpage
 type Background struct {
 }
 
+///Request needs to have the Path to the image to change in its header
 func (b *Background) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//get the img
-	imgData, err := png.Decode(r.Body)
+	imgData, err := png.Decode(r.Body) //decode the png image into Image type
 	if err != nil {
-		fmt.Println("Bad Image File. File must be a png")
+		http.Error(w, "Bad Image File. File must be a png", http.StatusBadRequest)
+		return
 	}
-	newbackground, err := os.Create(r.Header.Get("Path"))
+	//NOTE: The path to the image you are changing should be in the header of the
+	//request.
+	newbackground, err := os.Create(r.Header.Get("Path")) //create a place to store the file using the path of the old file
 	if err != nil {
-		fmt.Println("Bad Image File")
+		http.Error(w, "Bad Image File", http.StatusBadRequest)
+		return
 	}
-	png.Encode(newbackground, imgData)
+	png.Encode(newbackground, imgData) //encode the Image type to a png file at 'newbackground'
 	newbackground.Close()
 	w.WriteHeader(http.StatusNoContent)
 }
