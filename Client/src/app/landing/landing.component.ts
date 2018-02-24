921a13db32788466818a69323cc184c07b0edfe7
Christian <ctbrewton@users.noreply.github.com>
2018-02-23 23:27:02 -0500

    Revert "Ui polishing bb"

--- Client/src/app/landing/landing.component.ts
+++ Client/src/app/landing/landing.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Streamer } from '../models/Streamer.model';
 import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
 import { AdminService } from '../admin/admin.service';
-import { RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-landing',
@@ -17,7 +14,7 @@ export class LandingComponent implements OnInit {
   public user: string;
 
   constructor(public adminservice: AdminService,
-    private sanitizer: DomSanitizer , private router: Router) { }
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.setStreamers();
@@ -35,9 +32,5 @@ export class LandingComponent implements OnInit {
         });
       }
     );
-  }
-
-  public enterPress() {
-    this.router.navigate(['user', this.user]);
   }
 }
