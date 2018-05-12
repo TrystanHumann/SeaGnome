eefc663b00af032b467a13b684596f2a22306687
Christian Brewton <ctbrewton@yahoo.com>
2018-05-12 16:23:36 -0400

    updated so chat embed actually works with new embed endpoint and added view button (not wired up yet)

--- Client/src/app/landing/landing.component.ts
+++ Client/src/app/landing/landing.component.ts
@@ -17,6 +17,7 @@ export class LandingComponent implements OnInit {
   public streamers: Streamer[];
   public trustedUrl: Array<SafeUrl> = [];
   public trustedUrlChat: Array<SafeUrl> = [];
+  public streamerNames: Array<string> = [];
   public user: string;
   public ga: any;
   constructor(public adminservice: AdminService,
@@ -41,17 +42,22 @@ export class LandingComponent implements OnInit {
         }
         streamers.forEach(streamer => {
           // tslint:disable-next-line:max-line-length
-          this.trustedUrl.push(this.sanitizer.bypassSecurityTrustResourceUrl('http://player.twitch.tv/?channel=' + streamer.tag + '&muted=true'));
+          this.trustedUrl.push(this.sanitizer.bypassSecurityTrustResourceUrl('https://player.twitch.tv/?channel=' + streamer.tag + '&muted=true'));
         });
 
         streamers.forEach(streamer => {
           // tslint:disable-next-line:max-line-length
-          this.trustedUrlChat.push(this.sanitizer.bypassSecurityTrustResourceUrl('http://www.twitch.tv/' + streamer.tag + '/chat?darkpopout'));
+          this.trustedUrlChat.push(this.sanitizer.bypassSecurityTrustResourceUrl('http://www.twitch.tv/embed/' + streamer.tag + '/chat'));
+        });
+
+        streamers.forEach(streamer => {
+          // tslint:disable-next-line:max-line-length
+          this.streamerNames.push(streamer.tag);
         });
       }
     );
   }
-
+  
   public enterPress() {
     this.router.navigate(['user', this.user]);
   }
