56d25b4b86861d67b3307c318fc53d7cf35b66e5
Christian Brewton <ctbrewton@yahoo.com>
2018-05-17 01:20:11 -0400

    added much needed comments

--- Backend/handlers/backgroundUpload.go
+++ Backend/handlers/backgroundUpload.go
@@ -23,14 +23,6 @@ func (b *BackgroundUpload) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		fallthrough
 	case http.MethodPut:
-		// Fetching image
-		// imgData, err := png.Decode(r.Body)
-		// if err != nil {
-		// 	http.Error(w, "failed to read file", http.StatusBadRequest)
-		// }
-
-		// Determine path to background via a config file
-		// newbackground, err := os.Create(r.Header.Get("Path"))
 
 		// checking if file path folder exists for archive
 		_, err := os.Stat(path.Join(b.FilePath, "archive"))
@@ -45,6 +37,7 @@ func (b *BackgroundUpload) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		var buffer bytes.Buffer
 
+		// fetching image from formFile
 		file, _, err := r.FormFile("img")
 
 		if err != nil {
@@ -53,39 +46,33 @@ func (b *BackgroundUpload) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		// copy current image to archive
+		// copy current background image to archive folder with timestamp in nanoseconds
 		err = utils.Copy(path.Join(b.FilePath, "background.png"), path.Join(b.FilePath, "archive", fmt.Sprintf("background-%v.png", (time.Now().UnixNano()/1000000))))
 		if err != nil {
 			http.Error(w, "unable to archive previous background", http.StatusInternalServerError)
 			return
 		}
 
-		// Remove background
+		// Remove current background after it has been copied over
 		err = os.Remove(path.Join(b.FilePath, "background.png"))
 		if err != nil {
 			http.Error(w, "unable to remove previous background", http.StatusInternalServerError)
 			return
 		}
 
-		// Copy the file data to buffer
+		// copying contents of form file into our buffer
 		_, err = io.Copy(&buffer, file)
 		if err != nil {
 			http.Error(w, "unable to copy image to buffer", http.StatusInternalServerError)
 			return
 		}
 
+		// Creating a new background.png in file path using our buffer from form
 		err = utils.Create(buffer, path.Join(b.FilePath, "background.png"))
 		if err != nil {
 			http.Error(w, "unable to create new background image", http.StatusInternalServerError)
 			return
 		}
-		// if err != nil {
-		// 	http.Error(w, "failed to save file", http.StatusInternalServerError)
-		// 	return
-		// }
-		// png.Encode(newbackground, imgData)
-		// newbackground.Close()
-		// w.WriteHeader(http.StatusNoContent)
 		break
 	default:
 		http.Error(w, "method not found", http.StatusNotFound)
